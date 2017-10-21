e6b592d3a22226a0748bc5cb58b4f3641a69fa16
Riley Hanson <rileyh@bu.edu>
2017-10-20 21:30:24 -0400

    Game.java

--- Team4Good/app/src/main/java/com/example/tkixi/team4good/Game.java
+++ Team4Good/app/src/main/java/com/example/tkixi/team4good/Game.java
@@ -1,20 +1,29 @@
 package com.example.tkixi.team4good;
 
 /**
- * Created by rgasher on 10/20/2017.
+ * Created by Riley on 10/20/2017.
  */
 import java.util.Random;
 
 public class Game {
 
-    float ticker = 10.00
+    Random rand = new Random();
+    int theNum;
     String[] questions = {"something", "nothing", "hello"};
     String[][] answers = {{"ans1", "ans2", "ans3", "ans4"}, {"ans1", "ans2", "ans3", "ans4"}, {"ans1", "ans2", "ans3", "ans4"}};
 
-    String curQuest() {
+    private String curQuest() {
         Random rand = new Random();
         int  theNum = rand.nextInt(3) - 1;
         return questions[theNum];
+    }
+
+    private boolean Correct(int Choice) {
+        /**Checks the players choice **/
+        if (Choice == 0) {
+            return True;
+        }
+        return False;
     }
 
     
