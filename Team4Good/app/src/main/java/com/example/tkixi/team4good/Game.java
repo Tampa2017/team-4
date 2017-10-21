64cf2c41663df83c26e6d678ef4f59df47b6d182
Riley Hanson <rileyh@bu.edu>
2017-10-20 20:42:03 -0400

    Game.java

--- Team4Good/app/src/main/java/com/example/tkixi/team4good/Game.java
+++ Team4Good/app/src/main/java/com/example/tkixi/team4good/Game.java
@@ -3,6 +3,19 @@ package com.example.tkixi.team4good;
 /**
  * Created by rgasher on 10/20/2017.
  */
+import java.util.Random;
 
 public class Game {
+
+    String[] questions = {"something", "nothing", "hello"};
+    String[][] answers = {{"ans1", "ans2", "ans3", "ans4"}, {"ans1", "ans2", "ans3", "ans4"}, {"ans1", "ans2", "ans3", "ans4"}};
+
+    String curQuest() {
+        Random rand = new Random();
+        int  theNum = rand.nextInt(3) - 1;
+        return questions[theNum];
+    }
+
+
+
 }
