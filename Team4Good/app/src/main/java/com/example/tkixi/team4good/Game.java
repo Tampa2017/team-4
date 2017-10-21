92e858b8338381280186d342dcdf31a4bb3d2e47
Riley Hanson <rileyh@bu.edu>
2017-10-21 03:35:38 -0400

    Game.java

--- Team4Good/app/src/main/java/com/example/tkixi/team4good/Game.java
+++ Team4Good/app/src/main/java/com/example/tkixi/team4good/Game.java
@@ -63,6 +63,9 @@ public class Game extends AppCompatActivity implements GestureDetector.OnGesture
                 }
                 else {
                     p1.lives--;
+                    if (p1.lives == 0) {
+
+                    }
                     createButton();
                 }
             }
@@ -174,40 +177,6 @@ public class Game extends AppCompatActivity implements GestureDetector.OnGesture
         return false;
     }
 
-
-//    private void Game() {
-//        //this.Counter = 10;
-//    }
-//
-//    Random rand = new Random();
-//    int theNum;
-//    String[] questions = {"something", "nothing", "hello"};
-//    String[][] answers = {{"ans1", "ans2", "ans3", "ans4"}, {"ans1", "ans2", "ans3", "ans4"}, {"ans1", "ans2", "ans3", "ans4"}};
-//
-//    private String curQuest() {
-//        Random rand = new Random();
-//        int  theNum = rand.nextInt(3) - 1;
-//        return questions[theNum];
-//    }
-//    private boolean Correct(int Choice) {
-//        /**Checks the players choice **/
-//        if (Choice == 0) {
-//            return true;
-//        }
-//        return false;
-//    }
-
-
-
-//    private boolean Correct(int Choice) {
-//        /**Checks the players choice **/
-//        if (Choice == 0) {
-//            return True;
-//        }
-//        return False;
-//    }
-
-    
 
 
 
