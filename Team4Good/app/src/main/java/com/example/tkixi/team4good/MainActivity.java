8665ce5f6c7f4303d446a6e9e2029a8ef18b0270
Tkixi <tkixi@users.noreply.github.com>
2017-10-21 01:54:23 -0400

    mainpage

--- Team4Good/app/src/main/java/com/example/tkixi/team4good/MainActivity.java
+++ Team4Good/app/src/main/java/com/example/tkixi/team4good/MainActivity.java
@@ -1,13 +1,35 @@
 package com.example.tkixi.team4good;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button buttonSupport, buttonAbout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        buttonSupport = (Button) findViewById(R.id.buttonSupport);
+        buttonAbout = (Button) findViewById(R.id.buttonAbout);
+
+
+        buttonSupport.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, SupportActivity.class));
+            }
+        });
+
+        buttonAbout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, AboutActivity.class));
+            }
+        });
     }
 }
