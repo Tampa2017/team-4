c5b329afeaa6109eae1ae38af38a1b243a246883
Sudeep Swarnapuri <32913961+SudeepS97@users.noreply.github.com>
2017-10-20 19:48:24 -0500

    added ticker var

--- Team4Good/app/src/main/java/com/example/tkixi/team4good/Game.java
+++ Team4Good/app/src/main/java/com/example/tkixi/team4good/Game.java
@@ -7,6 +7,7 @@ import java.util.Random;
 
 public class Game {
 
+    float ticker = 10.00
     String[] questions = {"something", "nothing", "hello"};
     String[][] answers = {{"ans1", "ans2", "ans3", "ans4"}, {"ans1", "ans2", "ans3", "ans4"}, {"ans1", "ans2", "ans3", "ans4"}};
 
@@ -15,6 +16,8 @@ public class Game {
         int  theNum = rand.nextInt(3) - 1;
         return questions[theNum];
     }
+
+    
 
 
 
